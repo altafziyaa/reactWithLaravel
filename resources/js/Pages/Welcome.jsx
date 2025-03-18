fa393870230724e1cc0509841ebd8c1d46ba0351
md.altaf <altafziyaa91@gmail.com>
2025-03-18 20:25:01 +0530

    Initial commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,12 @@
+import First from '@/component/First'
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div>Welcome altaf
+        <First/>
+    </div>
+  )
+}
+
+export default Welcome
